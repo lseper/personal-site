61cf2cb6e173d79d7a309553f1a88b753edd78b9
Zaverose <seperliam@gmail.com>
2023-03-22 15:30:49 -0500

    mui blog starter code

--- personal-site/src/pages/_app.tsx
+++ personal-site/src/pages/_app.tsx
@@ -1,28 +1,32 @@
-import { ThemeProvider } from 'styled-components';
-import type { AppProps } from 'next/app'
-import { useEffect, useState } from 'react';
-import useDarkMode from "use-dark-mode"
-import { lightTheme, darkTheme, GlobalStyles } from "../theme/theme" 
+import { ThemeProvider } from "styled-components";
+import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
+import useDarkMode from "use-dark-mode";
+import { lightTheme, darkTheme, GlobalStyles } from "../theme/theme";
+import { Roboto } from "@next/font/google";
 
+const roboto = Roboto({
+	subsets: ["latin"],
+	weight: ["300", "400", "500", "700"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
+	const [isMounted, setIsMounted] = useState(false);
+	const darkmode = useDarkMode(true);
+	const theme = darkmode.value ? darkTheme : lightTheme;
 
-  const [isMounted, setIsMounted] = useState(false)
-  const darkmode = useDarkMode(true)
-  const theme = darkmode.value ? darkTheme : lightTheme
+	// don't flicker on SSR
+	useEffect(() => {
+		setIsMounted(true);
+	}, []);
 
-  // don't flicker on SSR
-  useEffect(() => {
-    setIsMounted(true)
-  }, [])
-
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <button onClick={darkmode.toggle}>Switch Mode</button>
-      <button onClick={darkmode.enable}>Dark Mode</button>
-      <button onClick={darkmode.disable}>Light Mode</button>
-      {isMounted && <Component {...pageProps} />}
-    </ThemeProvider>
-  )
+	return (
+		<ThemeProvider theme={theme}>
+			<GlobalStyles />
+			<button onClick={darkmode.toggle}>Switch Mode</button>
+			<button onClick={darkmode.enable}>Dark Mode</button>
+			<button onClick={darkmode.disable}>Light Mode</button>
+			{isMounted && <Component {...pageProps} />}
+		</ThemeProvider>
+	);
 }
--- personal-site/src/theme/theme.ts
+++ personal-site/src/theme/theme.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle} from "styled-components"
+import { createGlobalStyle } from "styled-components";
 
 // .theme {
 //     color: orangered;
@@ -6,29 +6,36 @@ import { createGlobalStyle} from "styled-components"
 //   }
 
 type AppTheme = {
-    color: string,
-    backgroundColor: string
-}
+	color: string;
+	backgroundColor: string;
+	fontFamily: string;
+};
 
-export const lightTheme : AppTheme = {
-    color: '#1e2328',
-    backgroundColor: 'orangered'
-}
+const ThemeBase = {
+	fontFamily: "roboto",
+};
 
-export const darkTheme : AppTheme = {
- color: 'orangered',
- backgroundColor: '#1e2328'
-}
+export const lightTheme: AppTheme = {
+	...ThemeBase,
+	color: "#1e2328",
+	backgroundColor: "orangered",
+};
 
-export const GlobalStyles = createGlobalStyle<{theme: AppTheme}>`
+export const darkTheme: AppTheme = {
+	...ThemeBase,
+	color: "orangered",
+	backgroundColor: "#1e2328",
+};
+
+export const GlobalStyles = createGlobalStyle<{ theme: AppTheme }>`
   body {
     background: ${({ theme }) => theme.backgroundColor};
     color: ${({ theme }) => theme.color};
-    font-family: system-ui, sans-serif;
-    transition: all 0.50s linear;
+    font-family: ${({ theme }) => theme.fontFamily};
+    transition: color 0.50s linear, background 0.5s linear;
   }
 
   p {
     line-height: 1.5;
   }
-`
+`;
