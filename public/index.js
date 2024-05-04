04044de2438f9f9ce41a5e899d1acad8c1cb8275
Liam Seper <lseper2@huskers.unl.edu>
2024-05-04 15:03:34 -0700

    blogs

--- public/index.js
+++ public/index.js
@@ -25,12 +25,43 @@ const thoughts = [
         <br />
         <p>But mentally, I can only think of it getting worse. Either that, or the whole cyber-chips-in-your-brain thing will happen, which is 
         much more terrifying. Despite science fiction, I don't think doing so nullifies the human experience. You could argue the same with any 
-        technological advancement; Do I, visiting my boyfriend in San Francisco, experience something "less human" than Gilgamesh did in Sumeria?</p>
-        <br />
-        <p>I don't think so. Such a comparison seems moot. I'm sure in thousands of years humans will awe and wonder in the mundane and the 
-        everyday I experience today, yet I would be stroking my ego to say I am Gilgamesh-esque.</p>
+        technological advancement; Do I, visiting my boyfriend in San Francisco in the year 2024, sitting in a padded, cushioned chair, typing on 
+        a laser-cut and harmoniously designed computer keyboard, while a 144hz monitor refreshes each and every pixel so I can see each and every character
+        I type, experience something "less human" than Gilgamesh did in Sumeria? In fact, I'd argue I experience something quite similar to that of the next 
+        generation of cyborg humans, and that my experience is much more alike theirs than Gilgamesh's.</p>
         `
     },
+    {
+        title: "That Obra Dinn sure can return!",
+        date: new Date(2024, 5, 1, 23, 28),
+        location: "Mountain View, CA",
+        content: `<p>My friend Sam asked if I wanted to play Return of the Obra Dinn, a game I'd seen through my studies of graphics programming.
+        I was initially excited by the art direction of the game, as the shader style seemed really cool. I really like the main game loop of discover ->
+        deduce -> confirm. It also presents to you radically strange content at it's peak, and then gives you the buildup - an interesting turn on things.<p>
+        <br />
+        <p>I will say the game is EXTREMELY difficult. I much enjoy playing it around friends, as I don't know if I'd have the patience nor mental energy to 
+        juggle a lot of the small detective work to do it! It's a decent jump outside my comfort zone, but I'm glad I took it, as it's been very interesting.</p>
+        <br />
+        <p>I'll say one of my favorite things is the incredible sound design of the game. The beginning of the flashback scenes with the narration and text on a 
+        simple uniform color background is incredible. I can tell each and every audio track was crafted with utmost care, as hearing the muffled voices, creaky boards
+        and swish and swash of the ocean really breathes life into the game.</p>
+        `
+    },
+    {
+        title: "Do not be afraid of yourself!",
+        date: new Date(2024, 5, 1, 23, 28),
+        location: "Mountain View, CA",
+        content: `<p>For the longest time I thought my obsessive intensity about things many people thought mundane, difficult, and uninteresting was bad. My friend Spottacus 
+        recently gave a <a href="https://www.youtube.com/watch?v=uE20gAvnru0&pp=ygUSc3BvdHRhY3VzIGJlcmtlbGV5">talk at Berkeley</a> that has resonated with my rather well.<p>
+        <br />
+        <p> For the longest time, I thought my feverish intensity and obsession was something I needed to reign in, but now I see that it's something I should <em>embrace</em>. 
+        So if you're reading this, embrace your interests! For the longest time I was afraid I was "missing out" on what life was "about" by obsessing over the things that 
+        interested me. But now I see that that is what life is to me. The joy of study and pursuit in itself are what I crave, and it's exhilirating to think of the possibilities 
+        that could come from that!</p>
+        <br />
+        <p>Carpe Diem!</p>
+        `
+    }
 ];
 
 window.addEventListener('resize', () => {
